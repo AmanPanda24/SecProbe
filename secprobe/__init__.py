5e573052fb8f0fc84d5c202ac6b6bdbe17298557
Aman Kumar Panda <amanpanda987@gmail.com>
2026-09-15 16:53:46 +0530

    Delete secprobe directory

--- secprobe/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-SecProbe - AI-Assisted Web Security Misconfiguration Scanner
-Author: Aman Kumar Panda
-Version: 1.0.0
-"""
-
-__version__ = "1.0.0"
-__author__ = "Aman Kumar Panda"
--- secprobe/modules/__init__.py
+++
@@ -1,1 +0,0 @@
-"""SecProbe scanning modules."""
